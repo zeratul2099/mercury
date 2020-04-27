14053c6271187d6350cbc2cecafadac586ebfb08
Marko <zeratul@posteo.de>
2020-04-27 20:43:59 +0200

    initial support for weatherbit.io-api

---
+++ src/bin/weatherbit.rs
@@ -0,0 +1,57 @@
+//#![feature(plugin, custom_derive)]
+#![allow(proc_macro_derive_resolution_fallback, dead_code)]
+#[macro_use]
+extern crate lazy_static;
+extern crate reqwest;
+#[macro_use]
+extern crate serde_derive;
+extern crate itertools;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate diesel;
+extern crate chrono;
+extern crate chrono_tz;
+extern crate time;
+
+#[path = "../common.rs"]
+mod common;
+#[path = "../weatherbit_model.rs"]
+mod weatherbit_model;
+#[path = "../models.rs"]
+mod models;
+#[path = "../schema.rs"]
+mod schema;
+
+//use self::models::insert_values;
+use common::{establish_connection, get_settings};
+use weatherbit_model::WeatherbitCurrent;
+use std::fs::File;
+
+fn main() {
+    let settings = get_settings();
+    let url = format!(
+        "https://api.weatherbit.io/v2.0/current?key={}&lat={}&lon={}",
+        settings.weatherbit_api_key, settings.lat_lon.0, settings.lat_lon.1
+    );
+    println!("{}", url);
+    let content: WeatherbitCurrent = reqwest::get(url.as_str()).unwrap().json().unwrap(); //.expect("weather request failed");
+    println!(
+        "t: {}, h: {}, {}, wspd: {}",
+        content.data.get(0).unwrap().temp,
+        content.data.get(0).unwrap().rh,
+        content.data.get(0).unwrap().weather.description,
+        content.data.get(0).unwrap().wind_spd
+    );
+    println!("{}", serde_json::to_string_pretty(&content).unwrap());
+    let file = File::create("weatherbitdump.json").unwrap();
+    serde_json::to_writer_pretty(&file, &content).unwrap();
+    /*let connection = establish_connection(&settings);
+    insert_values(
+        &connection,
+        &settings,
+        &0,
+        &content.currently.temperature,
+        &(content.currently.humidity * 100.0),
+    );*/
+}
---
+++ src/weatherbit_model.rs
@@ -0,0 +1,60 @@
+extern crate reqwest;
+extern crate yaml_rust;
+
+use chrono::{DateTime, Utc};
+
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct WeatherbitCurrent {
+    pub data: Vec<CurrentData>,
+    pub count: i64,
+}
+
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct CurrentData {
+    pub rh: i64,
+    pub pod: String,
+    pub lon: f64,
+    pub pred: f64,
+    pub timezone: String,
+    pub ob_time: DateTime<Utc>,
+    pub country_code: String,
+    pub clouds: i64,
+    pub ts: i64,
+    pub solar_rad: f64,
+    pub state_code: String,
+    pub city_name: String,
+    pub wind_spd: f64,
+    pub last_ob_time: DateTime<Utc>,
+    pub wind_cdir_full: String,
+    pub wind_cdir: String,
+    pub slp: f64,
+    pub vis: f64,
+    pub h_angle: f64,
+    pub suncast: String,
+    pub dni: f64,
+    pub dewpt: f64,
+    pub snow: i64,
+    pub uv: f64,
+    pub precip: i64,
+    pub wind_dir: i64,
+    pub sunrise: String,
+    pub ghi: f64,
+    pub dhi: f64,
+    pub aqi: i64,
+    pub lat: f64,
+    pub weather: CurrentDataWeather,
+    pub datetime: DateTime<Utc>,
+    pub temp: f64,
+    pub station: String,
+    pub elev_angle: f64,
+    pub app_temp: f64,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct CurrentDataWeather {
+    pub icon: String,
+    pub code: String,
+    pub description: String,
+}
