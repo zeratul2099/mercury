915b7f84897f52a7bb855dc4a5cced195e3422c6
Marko <zeratul@posteo.de>
2020-07-18 02:10:17 +0200

    try replace rocket with actix web

---
+++ src/bin/actix_ws.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+        })
+        .bind("0.0.0.0:6502")?
+        .run()
+        .await
+}
